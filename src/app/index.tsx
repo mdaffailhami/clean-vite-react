d9c91cf01a8ffcdff95afcab47a8954fe40b22b5
Daffa Ilhami <mdaffailhami@gmail.com>
2025-02-02 09:52:07 +0800

    Replace <a> with <Link>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,19 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export default function MainLayout() {
   return (
     <>
       <header>
         <nav className="flex items-center justify-around bg-orange-500 py-4 font-bold text-white">
-          <a href="/">
+          <Link to="/">
             <img className="h-8 w-8" src="/vite.svg" alt="logo" />
-          </a>
+          </Link>
           <ul className="flex items-center gap-4">
             <li>
-              <a href="/">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <a href="/count">Count</a>
+              <Link to="/count">Count</Link>
             </li>
           </ul>
         </nav>
@@ -21,13 +21,14 @@ export default function MainLayout() {
       <Outlet />
       <footer className="bg-gray-200 py-4 text-center text-sm text-gray-600">
         Created by{" "}
-        <a
-          href="https://mdaffailhami.github.io"
+        <Link
+          to="https://mdaffailhami.github.io"
           target="_blank"
           rel="noopener noreferrer"
+          className="font-bold italic underline"
         >
           Daffa Ilhami{" "}
-        </a>
+        </Link>
         with ❤️
       </footer>
     </>
